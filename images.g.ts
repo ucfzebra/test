ed70c3f6187b309adc6d926730955eb63c3a4391
Patrick M <90938071+ucfzebra@users.noreply.github.com>
2023-04-12 19:18:09 -0400

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,146 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "Blinka":
+            case "python":return img`
+. . . . . . a a a a . . . . . . 
+. . . . . a a a 1 9 a . . . . . 
+. . . . . a a a 1 8 a a . . . . 
+. . . . . a a a a a a c . . . . 
+. . . . . a a a 3 . . . 3 . . . 
+. . . . . . a a a 9 . . . . . . 
+. . . . . . . a a a 3 . . . . . 
+. . . . . . . . a a 9 9 . . . . 
+. . . . . . . . . a a 3 3 . . . 
+. . . . a a a a c c a 9 9 . . . 
+. . . 9 3 9 3 9 3 c a a 3 3 . . 
+. . a a a a a c c c a a 9 9 . . 
+. . 9 3 9 3 9 3 9 c a a 3 3 . . 
+a a a a a a c c c c c c 9 9 . . 
+a a a a a a a a a a a c c c . a 
+3 9 3 9 3 9 3 9 3 a a a a a a . 
+`;
+            case "image1":
+            case "myImage":return img`
+. 6 . . . . . . . . . . . . 6 . 
+6 9 6 . . . . . . . . . . 6 9 6 
+. 6 . . . . . . . . . . . . 6 . 
+. 6 . . . . . . . . . . . . 6 . 
+. 6 9 9 9 9 9 9 9 9 9 9 9 9 6 . 
+. 6 9 9 9 9 9 9 9 9 9 9 9 9 6 . 
+9 6 9 1 1 1 9 9 9 9 1 1 1 9 6 9 
+9 6 9 1 1 f 9 9 9 9 1 f 1 9 6 9 
+9 6 9 1 f f 9 9 9 9 f f 1 9 6 9 
+9 6 9 9 9 9 9 9 9 9 9 9 9 9 6 9 
+. 6 9 9 6 1 1 1 1 1 1 6 9 9 6 . 
+. . 9 9 9 6 6 6 6 6 6 9 9 9 . . 
+. . 9 9 9 9 9 9 9 9 9 9 9 9 . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "star":
+            case "anim1":return [img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . b . . . . . . . 
+. . . . . . . b d b . . . . . . 
+. . . . . . b 5 5 5 b . . . . . 
+. . . . . b b 5 5 5 b b . . . . 
+. . b b b b 5 5 5 1 1 b b b b . 
+. . b 5 5 5 5 5 5 1 1 5 5 5 b . 
+. . b d d 5 5 5 5 5 5 5 d d b . 
+. . . b d d 5 5 5 5 5 d d b . . 
+. . . c b 5 5 5 5 5 5 5 b c . . 
+. . . c b 5 5 5 5 5 5 5 b c . . 
+. . . c 5 5 d d b d d 5 5 c . . 
+. . . c 5 d d c c c d d 5 c . . 
+. . . c c c c . . . c c c c . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, img`
+. . . . . . . b b . . . . . . . 
+. . . . . . b d d b . . . . . . 
+. . . . . b d 5 5 d b . . . . . 
+. . . . b b 5 5 5 5 b b . . . . 
+. . . . b 5 5 5 5 5 5 b . . . . 
+b b b b b 5 5 5 5 1 1 d b b b b 
+b 5 5 5 5 5 5 5 5 1 1 1 5 5 5 b 
+b d d 5 5 5 5 5 5 1 1 1 5 d d b 
+. b d d 5 5 5 5 5 5 5 5 d d b . 
+. . b b 5 5 5 5 5 5 5 5 b b . . 
+. . c b 5 5 5 5 5 5 5 5 b c . . 
+. . c 5 5 5 5 d d 5 5 5 5 c . . 
+. . c 5 5 d b b b b d 5 5 c . . 
+. . c 5 d b c c c c b d 5 c . . 
+. . c c c c . . . . c c c c . . 
+. . . . . . . . . . . . . . . . 
+`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . b b . . . . . . . 
+. . . . . . b 5 5 b . . . . . . 
+. . . b b b 5 5 1 1 b b b . . . 
+. . . b 5 5 5 5 1 1 5 5 b . . . 
+. . . . b d 5 5 5 5 d b . . . . 
+. . . . c b 5 5 5 5 b c . . . . 
+. . . . c 5 d d d d 5 c . . . . 
+. . . . c 5 d c c d 5 c . . . . 
+. . . . c c c . . c c c . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, img`
+. . . . . . . . b . . . . . . . 
+. . . . . . b d d c . . . . . . 
+. . . . . b 1 1 d d c . . . . . 
+. . . . b 1 1 1 d 1 1 b . . . . 
+. . . . c 1 1 1 d 1 1 1 c c . . 
+b b b c d 1 1 c c 1 1 d 1 1 b b 
+b d 1 1 d d b c c c b d 1 1 1 b 
+b 1 1 1 1 c c . . c d d 1 1 1 b 
+b 1 1 1 1 c c . . b 1 1 d d c . 
+. b 1 1 d d b c b b 1 1 b c c . 
+. . c b d d b 1 1 b b d b c . . 
+. . c 1 1 d d 1 1 1 d d d b . . 
+. b d 1 1 1 d 1 1 d 1 1 1 d b . 
+. b d 1 1 1 d b b d 1 1 1 1 b . 
+. . b 1 1 d c c b b d 1 1 d b . 
+. . b b b b . . . b b b b b b . 
+`, img`
+. . . . . b b . . . . . . . . . 
+. . . . b 5 b b . . . . . . . . 
+. . b b 5 5 5 b b b . . . . . . 
+. b 5 5 5 5 5 5 5 b . . b . . . 
+. . b b 5 5 5 b b . . b 5 b . . 
+. . b 5 5 b 5 5 b . b 5 5 5 b . 
+. . b 5 b b b 5 b . . b 5 b . . 
+. . b b . . b b b . . b b b . . 
+. b 5 b b . . . . . b 5 b . . . 
+b 5 5 5 b b . . . b b 5 b b . . 
+. b 5 b b 5 b . b 5 5 5 5 5 b . 
+. b b b 5 5 5 b b b 5 5 5 b b . 
+. . b 5 5 5 5 5 b b 5 b 5 b . . 
+. . . b 5 5 5 b . . b b b . . . 
+. . . b 5 b 5 b . . . . . . . . 
+. . . b b b b b . . . . . . . . 
+`];
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
